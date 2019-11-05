8210f92c8281d4b04ee7f4050c339597afbeb2fe
MATTHEW SHAW <MS228668@falmouth.ac.uk>
2019-11-05 11:47:55 +0000

    Started adding scripts for loading and playing audio

---
+++ Assets/Scripts/SoundLoader.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundLoader : MonoBehaviour
+{
+	public float[] AudioData;
+	
+    public void ImportAudio(string path)
+	{
+		AudioData = new float[44100];
+		for(int i = 0; i < 44100; i++)
+		{
+			AudioData[i] = 1;
+		}
+	}
+}
---
+++ Assets/Scripts/SoundPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class SoundPlayer : MonoBehaviour
+{
+    public void PlayAudio(float[] data)
+	{
+		AudioSource source = GetComponent<AudioSource>();
+
+        AudioClip NewAudio = AudioClip.Create("GeneratedWave", data.Length, 1, 44100, false);
+        NewAudio.SetData(data, 0);
+        source.clip = NewAudio;
+
+        source.Play();
+	}
+}
