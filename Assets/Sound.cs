7446f58ed287a67b70d865b7bd5f5c1d1b54a60a
MATTHEW SHAW <MS228668@falmouth.ac.uk>
2019-11-05 09:32:49 +0000

    Pushed existing unity project (includes unfinished audio generation script)

---
+++ Assets/Sound.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Waveform {Sine, Sawtooth, Square}
+
+
+[RequireComponent(typeof(AudioSource))]
+public class Sound : MonoBehaviour
+{
+    private int position = 0;
+    public Waveform form = Waveform.Sine;
+    public int samplerate = 44100;
+    public float frequency = 440;
+    public float length = 2;
+    public bool Loop = false;
+    private int count = 0;
+
+    void Start()
+    {
+        AudioClip Audio = AudioClip.Create("MySinusoid", (int)(samplerate * length), 1, samplerate, true, OnAudioRead, OnAudioSetPosition);
+        AudioSource source = GetComponent<AudioSource>();
+        source.clip = Audio;
+        source.loop = Loop;
+        source.Play();
+    }
+
+    void OnAudioRead(float[] data)
+    {
+        
+        switch (form)
+        {
+            case Waveform.Sine:
+                while (count < data.Length)
+                {
+                    data[count] = Mathf.Sin(2 * Mathf.PI * frequency * position / samplerate);
+                    position++;
+                    count++;
+                }
+                break;
+
+            case Waveform.Sawtooth:
+                int sawLength = (int)(samplerate/frequency);
+                Debug.Log(sawLength);
+                int sawPosition = 0;
+
+                while (count < data.Length)
+                {
+                    data[count] = (float)sawPosition / sawLength;
+                    //Debug.Log("Position: " + sawPosition + ", length: " + sawLength + ", amplitude: " + (float)sawPosition / sawLength);
+                    position++;
+                    count++;
+                    sawPosition = position % sawLength;
+                    
+                }
+                break;
+
+            case Waveform.Square:
+                while (count < data.Length)
+                {
+                    data[count] = Mathf.Sin(2 * Mathf.PI * frequency * position / samplerate);
+                    position++;
+                    count++;
+                }
+                break;
+
+            default:
+                break;
+        }
+        
+    }
+
+    void OnAudioSetPosition(int newPosition)
+    {
+        position = newPosition;
+    }
+}
