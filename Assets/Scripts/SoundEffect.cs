1e0624bb20f34c5433c1499066410cb1f2564da9
MATTHEW SHAW <MS228668@falmouth.ac.uk>
2019-11-26 08:31:19 +0000

    Docstring pass on SoundEffect.cs

--- Assets/Scripts/SoundEffect.cs
+++ Assets/Scripts/SoundEffect.cs
@@ -3,9 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 
+/// <summary>
+/// Holds all the types of effects currently implemented
+/// </summary>
 public enum Effect {Riser, Double, Halve, RingMod};
 public class SoundEffect : MonoBehaviour
 {
+    /// <summary>
+    /// Adds a rising volume effect to the audio
+    /// </summary>
+    /// <param name="data">The sound to be affected</param>
+    /// <returns>The affected sound</returns>
     public static float[] Riser(float[] data)
     {
         int SampleLength = data.Length;
@@ -18,12 +26,22 @@ public class SoundEffect : MonoBehaviour
         return data;
     }
 
+    /// <summary>
+    /// Doubles the frequency of the sound
+    /// </summary>
+    /// <param name="data">The sound to be affected</param>
+    /// <returns>The affected sound</returns>
     public static float[] Double(float[] data)
     {
         data = data.Where((source, index) => (index / 2) * 2 == index).ToArray();
         return data;
     }
 
+    /// <summary>
+    /// Halves the frequency of the sound
+    /// </summary>
+    /// <param name="data">The sound to be affected</param>
+    /// <returns>The affected sound</returns>
     public static float[] Halve(float[] data)
     {
         float[] ret = new float[data.Length * 2];
@@ -37,6 +55,11 @@ public class SoundEffect : MonoBehaviour
         return ret;
     }
 
+    /// <summary>
+    /// Adds a ring modulation effect with 300hz as the modulation frequency
+    /// </summary>
+    /// <param name="data">The sound to be affected</param>
+    /// <returns>The affected sound</returns>
     public static float[] RingMod (float[] data)
     {
         float[] ret = new float[data.Length];
